68a369f38c2d6d9cff3f41d206f0a8d1487bf4c6
0xMindGuy <coachbolatunji@gmail.com>
2023-05-14 19:47:48 +0100

    create password generator function

--- main_pw_manager.py
+++ main_pw_manager.py
@@ -1,12 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(0, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(0, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(0, 10))]
+
+    password_list = password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    pass_entry.insert(0, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
 def save():
     website = website_entry.get()
     email = email_entry.get()
@@ -58,7 +76,7 @@ pass_entry = Entry(width=33)
 pass_entry.grid(row=3, column=1)
 
 # Button
-generate_pw_button = Button(text="Generate Password")
+generate_pw_button = Button(text="Generate Password", command=generate_password)
 generate_pw_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=44, command=save)
---
+++ pw_generator_code.py
@@ -0,0 +1,35 @@
+#Password Generator Project
+from random import choice, randint, shuffle
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+password_letters = [choice(letters) for _ in range(randint(0, 10))]
+password_numbers = [choice(numbers) for _ in range(randint(0, 10))]
+password_symbols = [choice(symbols) for _ in range(randint(0, 10))]
+
+
+password_list = password_letters + password_numbers + password_letters
+shuffle(password_list)
+
+password = "".join(password_list)
+
+print(f"Your password is: {password}")
+
+# nr_letters = random.randint(8, 10)
+# nr_symbols = random.randint(2, 4)
+# nr_numbers = random.randint(2, 4)
+
+# for char in range(nr_letters):
+#   password_list.append(random.choice(letters))
+#
+# for char in range(nr_symbols):
+#   password_list += random.choice(symbols)
+#
+# for char in range(nr_numbers):
+#   password_list += random.choice(numbers)
+#
+# random.shuffle(password_list)
+
+
